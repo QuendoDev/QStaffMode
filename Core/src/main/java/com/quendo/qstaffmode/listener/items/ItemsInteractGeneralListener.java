f110260d5124de3fca4d200f32f9def63be4a663
QuendoDev <unconfigured@null.spigotmc.org>
2022-08-09 15:22:01 +0200

    added returns to avoid event to keep checking conditionals

--- Core/src/main/java/com/quendo/qstaffmode/listener/items/ItemsInteractGeneralListener.java
+++ Core/src/main/java/com/quendo/qstaffmode/listener/items/ItemsInteractGeneralListener.java
@@ -14,9 +14,11 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 import team.unnamed.inject.InjectAll;
 
 import javax.inject.Named;
@@ -67,6 +69,12 @@ public class ItemsInteractGeneralListener implements Listener {
     @EventHandler
     public void onAction(PlayerInteractEvent e) {
         Player p = e.getPlayer();
+        if (e.getClickedBlock() != null &&
+                (e.getClickedBlock().getType() == Material.CHEST ||
+                        e.getClickedBlock().getType() == Material.TRAPPED_CHEST)) {
+            return;
+        }
+
         if (staffModeManager.isInStaffMode(p)) {
             if (p.hasPermission("qstaffmode.useitems") && e.getItem() != null && utils.getItemInHand(e) != null) {
 
@@ -89,6 +97,7 @@ public class ItemsInteractGeneralListener implements Listener {
                         }
                     }
                     e.setCancelled(true);
+                    return;
                 }
 
                 ////////////******STAFFLIST******/////////////
@@ -97,6 +106,7 @@ public class ItemsInteractGeneralListener implements Listener {
                         Bukkit.getServer().getPluginManager().callEvent(new StaffListInteractEvent(p));
                     }
                     e.setCancelled(true);
+                    return;
                 }
 
                 ////////////******VANISH******/////////////
@@ -105,6 +115,7 @@ public class ItemsInteractGeneralListener implements Listener {
                         Bukkit.getServer().getPluginManager().callEvent(new VanishInteractEvent(p));
                     }
                     e.setCancelled(true);
+                    return;
                 }
 
                 ////////////******FLY******/////////////
@@ -113,6 +124,7 @@ public class ItemsInteractGeneralListener implements Listener {
                         Bukkit.getServer().getPluginManager().callEvent(new FlyInteractEvent(p));
                     }
                     e.setCancelled(true);
+                    return;
                 }
 
                 ////////////******RANDOMTP******/////////////
@@ -121,15 +133,40 @@ public class ItemsInteractGeneralListener implements Listener {
                         Bukkit.getServer().getPluginManager().callEvent(new RandomTpInteractEvent(p));
                     }
                     e.setCancelled(true);
+                    return;
                 }
 
                 ////////////******KBFISHINGROD******/////////////
                 if (itemInHandequalsItem(e, "kbFishingRod") && p.hasPermission("qstaffmode.items.kbfishingrod")) {
                     MessageUtil.sendMessage(p, messages.getString("usedFishingRod"));
+                    return;
+                }
+            }
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void attack (EntityDamageByEntityEvent event) {
+        if (event.getDamager() instanceof Player && event.getEntity() instanceof Player) {
+            Player attacker = (Player) event.getDamager();
+            Player target = (Player) event.getEntity();
+            if (staffModeManager.isInStaffMode(attacker) && !staffModeManager.isInStaffMode(target)) {
+                if (itemInHandequalsItem(utils.getItemInHand(attacker), "kbFishingRod") &&
+                        attacker.hasPermission("qstaffmode.items.kbfishingrod")) {
+                    target.setVelocity(attacker.getLocation().getDirection().multiply(3));
                 }
             }
         }
     }
+
+    private boolean itemInHandequalsItem (ItemStack itemStack, String key) {
+        boolean metaAndName = itemStack.hasItemMeta() && itemStack.getItemMeta().hasDisplayName();
+        boolean sameName = itemStack.hasItemMeta() && itemStack.getItemMeta().getDisplayName().equals(items.getString("items." + key + ".name"));
+        boolean sameId = utils.getMaterial(itemStack.getType().name()) == utils.getMaterial(items.getString("items." + key + ".id"));
+        return metaAndName && sameName && sameId;
+    }
+
     private boolean itemInHandequalsItem (PlayerEvent e, String key) {
         boolean metaAndName = utils.getItemInHand(e).hasItemMeta() && utils.getItemInHand(e).getItemMeta().hasDisplayName();
         boolean sameName = utils.getItemInHand(e).hasItemMeta() && utils.getItemInHand(e).getItemMeta().getDisplayName().equals(items.getString("items." + key + ".name"));
